a6f1437ad0122c02c5bbf9ef271dd19092f5492e
Anhnhi Tran <anhtran811@gmail.com>
2023-02-22 15:36:48 -0700

    Created users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name {Faker::Name::first_name}
+    last_name {Faker::Name::last_name}
+    phone_numbner {Faker::PhoneNumber.cell_phone}
+    email {Faker::Internet.email("#{first_name}.#{last_name}")}
+    password {Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3)}
+  end
+end
