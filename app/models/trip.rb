5b346b270274aedb90ea0b832bb8f44f4b119685
Matisse Mallette <matissemallette@gmail.com>
2023-02-23 12:19:35 -0700

    Fix schema

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,5 @@ class Trip < ApplicationRecord
   has_many :trip_attendees, dependent: :destroy
   has_many :trip_events, dependent: :destroy
 
-  validates_presence_of :name, :city, :country, :postcode, :place_id
+  validates_presence_of :name, :city, :country, :postcode
 end
