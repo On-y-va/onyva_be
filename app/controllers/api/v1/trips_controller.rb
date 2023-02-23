75d419a32d434c3377d4431a475da11cc242569c
Matisse Mallette <matissemallette@gmail.com>
2023-02-23 11:58:36 -0700

    Complete trips update endpoint

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -12,6 +12,12 @@ class Api::V1::TripsController < ApplicationController
     render json: TripSerializer.new(trip), status: :created
   end
 
+  def update
+    trip = Trip.find(params[:id])
+    trip.update!(trip_params)
+    render json: TripSerializer.new(trip)
+  end
+
   private
 
   def trip_params
