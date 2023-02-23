61957375f89a1a3ef356f789a12bb3d4dc28c447
Anhnhi Tran <anhtran811@gmail.com>
2023-02-23 14:10:48 -0700

    Updated trips factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
     city {Faker::Address.city}
     country {Faker::Address.country}
     postcode {Faker::Address.postcode}
-    place_id {Faker::Alphanumeric.alphanumeric(number: 15, min_alpha: 3, min_numeric: 3)}
   end
 end
