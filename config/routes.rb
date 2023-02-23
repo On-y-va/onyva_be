4d6c6c278c3d2cdfae496806df82163b4d82c0f0
Anhnhi Tran <anhtran811@gmail.com>
2023-02-23 12:28:48 -0700

    Fixes errors from schema change

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update, :destroy]
-<<<<<<< HEAD
-      resources :trips, only: [:index, :show, :create, :destroy]
-=======
-      resources :trips, only: [:index, :show, :create, :update]
->>>>>>> 00ce15b46be2b7c7f9447efa791213deeb3e7d49
+      resources :trips, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
