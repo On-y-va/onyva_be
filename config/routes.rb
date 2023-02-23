f7c4c92beac5edea3bac62e6e8e381121a8d3b20
Anhnhi Tran <anhtran811@gmail.com>
2023-02-22 22:16:32 -0700

    Completed happy path testing for trip show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show, :index, :create, :update]
+      resources :trips, only: [:show]
     end
   end
 end
