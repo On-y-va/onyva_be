67e531f0635f33c1ecc00d21da4d5ca7ea9366b3
Anhnhi Tran <anhtran811@gmail.com>
2023-02-23 12:41:03 -0700

    Completed happy and sad path testing for trips delete endpoints

--- spec/requests/api/v1/trips/destroy_request_spec.rb
+++ spec/requests/api/v1/trips/destroy_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Trips Delete API' do
   describe 'DELETE /trips/:id' do
     context 'if a trip exists' do
-      it 'can delete a trip' do
+      it 'can delete a trip without deleting the users associated with this trip' do
         user_1 = create(:user)
         user_2 = create(:user)
         user_3 = create(:user)
@@ -29,6 +29,20 @@ describe 'Trips Delete API' do
         expect(User.count).to eq(5)
         expect(Trip.count).to eq(1)
         expect(TripAttendee.count).to eq(3)
+      end
+    end
+
+    context 'if the trip does not exist' do
+      it 'sends an error message' do
+        trip = create(:trip)
+
+        delete "/api/v1/trips/#{Trip.last.id+1}"
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(404)
+        expect(response_body[:error]).to match(/Couldn't find Trip with 'id'=#{Trip.last.id+1}/)
+        expect(Trip.count).to eq(1)
       end
     end
   end
