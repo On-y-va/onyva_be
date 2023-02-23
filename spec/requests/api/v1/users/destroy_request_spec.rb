32bec57af1b5b7f361ea90b354f0e2f40a8a6f67
Anhnhi Tran <anhtran811@gmail.com>
2023-02-23 10:43:11 -0700

    Completed sad path testing for users delete endpoint

--- spec/requests/api/v1/users/destroy_request_spec.rb
+++ spec/requests/api/v1/users/destroy_request_spec.rb
@@ -2,17 +2,34 @@ require 'rails_helper'
 
 describe 'Users Delete API' do
   describe 'DELETE /users:/id' do
-    it 'can delete a user' do
-      user = create(:user)
+    context 'if the user exists' do 
+      it 'can delete a user' do
+        user = create(:user)
 
-      expect(User.count).to eq(1)
-      
-      delete "/api/v1/users/#{user.id}"
-      
-      expect(response).to be_successful
-      expect(response.status).to eq(204)
-      expect(User.count).to eq(0)
-      expect{User.find(user.id)}.to raise_error(ActiveRecord::RecordNotFound)
+        expect(User.count).to eq(1)
+        
+        delete "/api/v1/users/#{user.id}"
+        
+        expect(response).to be_successful
+        expect(response.status).to eq(204)
+        expect(User.count).to eq(0)
+        expect{User.find(user.id)}.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'if the user does not exist' do
+      it 'sends an error message' do
+        user = create(:user)
+
+        delete "/api/v1/users/#{User.last.id+1}"
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        require 'pry'; binding.pry
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(404)
+        expect(response_body[:error]).to match(/Couldn't find User with 'id'=#{User.last.id+1}/)
+        expect(User.count).to eq(1)
+      end
     end
   end
 end
