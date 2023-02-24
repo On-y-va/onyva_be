084d3a0610072e7b1c886bf861bcf59bce0ece7a
Anhnhi Tran <anhtran811@gmail.com>
2023-02-23 18:34:22 -0700

    Get city info service passing

---
+++ app/services/trip_events_service.rb
@@ -0,0 +1,16 @@
+class TripEventsService
+  def self.get_city_info(city)
+    response = conn.get("/v1/geocode/search?text=#{city}&lang=en&limit=10&type=city&apiKey=#{ENV['GEOAPIFY_KEY']}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+
+  private 
+
+  def self.conn
+    Faraday.new(
+      url: "https://api.geoapify.com",
+      params: { apiKey: ENV['GEOAPIFY_KEY']}
+    )
+  end
+end
--- spec/services/trip_events_service.rb
+++ spec/services/trip_events_service.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TripEventsService do
-  it 'can return information based off a city' do
+  it 'can return information based off a city', :vcr do
     city_search = TripEventsService.get_city_info("Barcelona")
-
     expect(city_search).to be_a(Hash)
-    expect(city_search).to have_key(:data)
+    expect(city_search).to have_key(:features)
+    expect(city_search[:features]).to be_an(Array)
+    
+    city = city_search[:features][0]
+
+    expect(city).to have_key(:properties)
+    expect(city[:properties]).to be_a(Hash)
+
+    expect(city[:properties]).to have_key(:country)
+    expect(city[:properties][:country]).to be_a(String)
+
+    expect(city[:properties]).to have_key(:city)
+    expect(city[:properties][:city]).to be_a(String)
+
+    expect(city[:properties]).to have_key(:postcode)
+    expect(city[:properties][:postcode]).to be_a(String)
+
+    expect(city[:properties]).to have_key(:place_id)
+    expect(city[:properties][:place_id]).to be_a(String)
   end
 end
