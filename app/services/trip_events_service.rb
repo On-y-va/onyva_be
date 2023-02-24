a22634b1cb40aca8f5a4a8b5596627606e0636a4
Anhnhi Tran <anhtran811@gmail.com>
2023-02-24 16:32:06 -0700

    Completed restaurant search service

--- app/services/trip_events_service.rb
+++ app/services/trip_events_service.rb
@@ -3,8 +3,14 @@ class TripEventsService
     response = conn.get("/v1/geocode/search?text=#{city}&country#{country}&lang=en&limit=10&type=city&apiKey=#{ENV['GEOAPIFY_KEY']}")
     JSON.parse(response.body, symbolize_names: true)
   end
-
   
+  def self.get_restaurants(place_id)
+    # response = conn.get("v2/places?categories=catering,catering.restaurant&filter=circle:2.1774322,41.3828939,5000&bias=proximity:2.1774322,41.3828939&lang=en&limit=15&apiKey=#{ENV['GEOAPIFY_KEY']}")
+    response = conn.get("/v2/places?categories=catering,catering.restaurant&filter=place:#{place_id}&lang=en&limit=15&apiKey=#{ENV['GEOAPIFY_KEY']}")
+    JSON.parse(response.body, symbolize_names: true)
+ 
+  end
+
   private 
 
   def self.conn
