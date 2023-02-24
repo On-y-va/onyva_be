ee67782d14c3cb6bf8f955fca6d0e0db559c276a
Anhnhi Tran <anhtran811@gmail.com>
2023-02-24 11:44:48 -0700

    Updated associations for trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   has_many :trip_attendees, dependent: :destroy
   has_many :trip_events, dependent: :destroy
+  has_many :users, through: :trip_attendees, dependent: :destroy
 
   validates_presence_of :name, :city, :country, :postcode, :start_date, :end_date
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Trip, type: :model do
   describe 'relationships' do
     it { should have_many(:trip_events) }
     it { should have_many(:trip_attendees) }
+    it { should have_many(:users).through(:trip_attendees) }
   end
 
   describe 'validations' do
