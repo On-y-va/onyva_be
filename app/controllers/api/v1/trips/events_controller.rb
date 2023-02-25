f7fece9d23719d44b57fe9eecffd5bfdd22cce73
Anhnhi Tran <anhtran811@gmail.com>
2023-02-24 20:56:26 -0700

    WIP: events controller, commit to push migrations to main

--- app/controllers/api/v1/trips/events_controller.rb
+++ app/controllers/api/v1/trips/events_controller.rb
@@ -2,6 +2,13 @@ class Api::V1::Trips::EventsController < ApplicationController
   def index
     trip = Trip.find(params[:id])
     city = CityFacade.get_city_info(trip.city, trip.country, trip.postcode)
-    trip_events = CityFacade.get_restaurant(city.place_id)
+    trip_poros = CityFacade.get_restaurant(city.place_id)
+    
+    render json: TripEventsSerializer(trip_events)
+    # make poros into models
+    # iterate through trip poros? 
+    # trips_poros.each do |poro|
+    #   TripEvent.create!(trip_id: trip.id, event_date: "something", event_time: "something", event_id: poro.place_id)
+    # end
   end
 end
