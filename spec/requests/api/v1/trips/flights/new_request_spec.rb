e37a1c461c6d1ead74f4871f44a940004e3750e5
Anhnhi Tran <anhtran811@gmail.com>
2023-02-25 12:56:22 -0700

    Added create flight for trip endpoint

---
+++ spec/requests/api/v1/trips/flights/new_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Flights create API' do
+  describe 'POST /trips' do
+    context 'when a flight is successfully created' do
+      it 'can create a new flight' do
+        user = create(:user)
+        trip = create(:trip)
+
+        flight_params = ({
+          user_email: user.email,
+          airline_code: "SW",
+          flight_number: "1234",
+          date: Date.today + 30.days
+          })
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        post "/api/v1/trips/#{trip.id}/flights", headers: headers, params: JSON.generate(flight: flight_params)
+
+        new_flight = Flight.last
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        expect(new_flight.airline_code).to eq(flight_params[:airline_code])
+        expect(new_flight.flight_number).to eq(flight_params[:flight_number])
+        expect(new_flight.date).to eq(flight_params[:date])
+      end
+    end
+  end
+end
