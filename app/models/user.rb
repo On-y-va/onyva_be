1133f2513db4f530b891d491dd491b9408ada2a3
Anhnhi Tran <anhtran811@gmail.com>
2023-02-26 14:54:29 -0700

    FIxed  user new tests after google uid migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :trip_attendees, dependent: :destroy
   has_many :trips, through: :trip_attendees, dependent: :destroy
 
-  validates_presence_of :first_name, :last_name, :email, :password_digest
+  validates_presence_of :first_name, :last_name, :email, :google_uid
   validates_uniqueness_of :email
-  has_secure_password
+  # has_secure_password
 
   def self.find_user_by_email(email)
     self.where("email = ?", "#{email}")
