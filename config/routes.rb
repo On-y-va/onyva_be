c2fc6a9c25f373357993675c98fa62d7216726e8
Braxton Eddings <beddings81@gmail.com>
2023-02-26 16:30:57 -0600

    Complete find_all trips by status endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/users/find', to: 'users_search#show'
-      get '/users/:id/trips/find_all', to: 'users/trips/find_all#show'
+      get '/users/:id/trips/find_all', to: 'users/trips/find_all#index'
 
       resources :users, only: [:index, :show, :create, :update, :destroy] do
         resources :trips, only: [:index], controller: 'users/trips'
