aa1fa7b4b617c8e0eac654351638cfe8d729e18e
Anhnhi Tran <anhtran811@gmail.com>
2023-02-25 23:07:54 -0700

    General clean up

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,5 @@
 class Flight < ApplicationRecord
   belongs_to :user
-  # belongs_to :trip
-  # has_many :trips, through: :user
+  
   validates_presence_of :airline_code, :flight_number, :date
 end
