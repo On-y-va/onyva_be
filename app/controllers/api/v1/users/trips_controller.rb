ab07e598fe4e9bdee4a90dfc363657718a2d4ccd
Matisse Mallette <matissemallette@gmail.com>
2023-02-27 17:40:58 -0700

    Complete user trips update?

--- app/controllers/api/v1/users/trips_controller.rb
+++ app/controllers/api/v1/users/trips_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::Users::TripsController < ApplicationController
-  def index
-    render json: TripSerializer.new(User.find(params[:user_id]).trips)
+  def update
+    trip = User.find(params[:user_id]).trips.find(params[:id])
+    trip.update!(user_trip_params)
+    render json: TripSerializer.new(trip)
+  end
+
+  private
+
+  def user_trip_params
+    params.require(:trip).permit(:name, :city, :country, :postcode, :start_date, :end_date).merge(place_id: @place_id, image_url: @url)
   end
 end
