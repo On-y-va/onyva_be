f0c39d28002726ec555e2a5d141d2779107fbf73
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-28 16:18:52 -0800

    Reformat

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -32,7 +32,6 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def update
-    
     trip = Trip.find(params[:id])
     trip.update!(trip_params)
     render json: TripSerializer.new(trip)
