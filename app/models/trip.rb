aeade7a0691467852461638e8a8776160baa34af
Braxton Eddings <beddings81@gmail.com>
2023-03-01 18:02:34 -0600

    Add custom validation tro trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,10 @@
 class Trip < ApplicationRecord
+  include ActiveModel::Validations
   has_many :trip_attendees, dependent: :destroy
   has_many :users, through: :trip_attendees, dependent: :destroy
   has_many :events, dependent: :destroy
   has_many :flights, through: :users, dependent: :destroy
-
+  
   validates_presence_of :name, :city, :country, :postcode, :start_date, :end_date
+  validates_with TripValidator
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe Trip, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:country) }
     it { should validate_presence_of(:postcode) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+
+    describe 'custom validations' do
+      describe '#end_date_after_start_date?' do
+        it 'doesnt create a trip if the dates are not valid' do
+          trip = Trip.create(city: "mboro", country: "United states", name: "test", postcode: "123456", start_date: "12/12/23", end_date: "12/1/23")
+
+          expect(Trip.exists?(trip.id)).to eq(false)
+        end
+      end
+    end
   end
 end
