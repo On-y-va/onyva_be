258607fc28d0db26816905ee340c0a370e4c4b52
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-02 13:40:21 -0800

    Update city facade spec

--- app/helpers/trip_helper.rb
+++ app/helpers/trip_helper.rb
@@ -1,7 +1,7 @@
 module TripHelper
   def location_info(trip)
     place_id = CityFacade.get_city_info(*location_params.values)
-    url = FlickrFacade.get_city_image(params[:trip][:city], params[:trip][:country]).url
+    url = FlickrFacade.get_city_image(params[:trip][:city], params[:trip][:country])
     trip.update!(place_id: place_id, image_url: url)
     create_events(place_id, trip.id)
   end
