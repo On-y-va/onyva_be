99c2599a9874ba842e0ca7ea5a6d4c0ea473c99f
Braxton Eddings <beddings81@gmail.com>
2023-03-01 20:52:08 -0600

    Trips controller refactor WIP

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -8,23 +8,35 @@ class Api::V1::TripsController < ApplicationController
     options = {include: [:events]}
     render json: TripSerializer.new(trip, options)
   end
+
+  def location_info
+    city_info = CityFacade.get_city_info(*location_params.values)
+
+    @place_id = city_info.place_id
+    
+    image = FlickrFacade.get_city_image(params[:trip][:city], params[:trip][:country])
+    @url = image.url
+    create_events
+  end
   
+  def create_events
+    restaurants = CityFacade.get_restaurant_info(city_info.place_id)
+    restaurants.each do |restaurant|
+      Event.create!(trip_id: trip.id, event_id: restaurant.place_id, name: restaurant.name, address: restaurant.address, category: 0)
+    end
+
+    attractions = CityFacade.get_tourist_attraction_info(city_info.place_id)
+    attractions.each do |attraction|
+      Event.create!(trip_id: trip.id, event_id: attraction.place_id, name: attraction.name, address: attraction.address, category: 1)
+    end
+
+  end
   def create
+    require 'pry'; binding.pry
     user = User.find(params[:trip][:user_id])
     trip = user.trips.create!(trip_params)
-      city_info = CityFacade.get_city_info(params[:trip][:city], params[:trip][:country], params[:trip][:postcode])
-      @restaurants = CityFacade.get_restaurant_info(city_info.place_id)
-      @attractions = CityFacade.get_tourist_attraction_info(city_info.place_id)
-      @place_id = city_info.place_id
-      image = FlickrFacade.get_city_image(params[:trip][:city], params[:trip][:country])
-      @url = image.url
-      @restaurants.each do |restaurant|
-        Event.create!(trip_id: trip.id, event_id: restaurant.place_id, name: restaurant.name, address: restaurant.address, category: 0)
-      end
-      @attractions.each do |attraction|
-        Event.create!(trip_id: trip.id, event_id: attraction.place_id, name: attraction.name, address: attraction.address, category: 1)
-      end
-      render json: TripSerializer.new(trip), status: :created
+    trip.location_info
+    render json: TripSerializer.new(trip), status: :created
   end
 
   def update
@@ -42,5 +54,9 @@ class Api::V1::TripsController < ApplicationController
 
   def trip_params
     params.require(:trip).permit(:name, :city, :country, :postcode, :start_date, :end_date).merge(place_id: @place_id, image_url: @url)
+  end
+
+  def location_params
+    params.require(:trip).permit(:city, :country, :postcode)
   end
 end
