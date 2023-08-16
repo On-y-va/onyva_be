0fff384bc488f4419bd2ae59cf337d1ac0aad83d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-08-16 15:18:22 -0600

    Reformat test

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -25,37 +25,37 @@ RSpec.describe Trip, type: :model do
         end
       end
     end
+  end
 
-    describe 'instance methods' do
-      describe '#users_going' do
-        it 'returns a list of users who accepted the trip invitation' do
-          trip = create(:trip)
+  describe 'instance methods' do
+    describe '#users_going' do
+      it 'returns a list of users who accepted the trip invitation' do
+        trip = create(:trip)
 
-          user = create(:user)
-          user2 = create(:user)
-          user3 = create(:user)
-          user4 = create(:user)
+        user = create(:user)
+        user2 = create(:user)
+        user3 = create(:user)
+        user4 = create(:user)
 
-          TripAttendee.create(trip_id: trip.id, user_id: user.id, status: 1)
-          TripAttendee.create(trip_id: trip.id, user_id: user2.id, status: 1)
-          TripAttendee.create(trip_id: trip.id, user_id: user3.id, status: 0)
-          TripAttendee.create(trip_id: trip.id, user_id: user4.id, status: 0)
+        TripAttendee.create(trip_id: trip.id, user_id: user.id, status: 1)
+        TripAttendee.create(trip_id: trip.id, user_id: user2.id, status: 1)
+        TripAttendee.create(trip_id: trip.id, user_id: user3.id, status: 0)
+        TripAttendee.create(trip_id: trip.id, user_id: user4.id, status: 0)
 
-          expect(trip.users_going).to eq([user, user2])
-          expect(trip.users_going).to_not include(user3)
-        end
+        expect(trip.users_going).to eq([user, user2])
+        expect(trip.users_going).to_not include(user3)
       end
+    end
 
-      describe '#most_popular_event' do
-        it 'returns the event with the most votes' do
-          trip = create(:trip)
+    describe '#most_popular_event' do
+      it 'returns the event with the most votes' do
+        trip = create(:trip)
 
-          event = create(:event, trip_id: trip.id, votes: 0)
-          event2 = create(:event, trip_id: trip.id, votes: 3)
-          event3 = create(:event, trip_id: trip.id, votes: 5)
+        event = create(:event, trip_id: trip.id, votes: 0)
+        event2 = create(:event, trip_id: trip.id, votes: 3)
+        event3 = create(:event, trip_id: trip.id, votes: 5)
 
-          expect(trip.most_popular_event).to eq(event3)
-        end
+        expect(trip.most_popular_event).to eq(event3)
       end
     end
   end
